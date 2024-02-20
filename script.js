b8ff10059ec57bf483b9fdc50d3f665bdd0812ff
OBIANUKA CHIMAOBI <obianukachimaobim@gmail.com>
2024-02-21 00:08:05 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,134 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const counterElement = document.getElementById("span80");
+  const targetNumber = 80;
+  const duration = 1500; // Animation duration in milliseconds
+  const interval = duration / targetNumber;
+
+  let count = 0;
+
+  const incrementCounter = () => {
+    counterElement.textContent = count;
+    count++;
+
+    if (count <= targetNumber) {
+      setTimeout(incrementCounter, interval);
+    }
+  };
+
+  incrementCounter();
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const counterElement = document.getElementById("span85");
+  const targetNumber = 85;
+  const duration = 1800; // Animation duration in milliseconds
+  const interval = duration / targetNumber;
+
+  let count = 0;
+
+  const incrementCounter = () => {
+    counterElement.textContent = count;
+    count++;
+
+    if (count <= targetNumber) {
+      setTimeout(incrementCounter, interval);
+    }
+  };
+
+  incrementCounter();
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const counterElement = document.getElementById("span75");
+  const targetNumber = 75;
+  const duration = 2000; // Animation duration in milliseconds
+  const interval = duration / targetNumber;
+
+  let count = 0;
+
+  const incrementCounter = () => {
+    counterElement.textContent = count;
+    count++;
+
+    if (count <= targetNumber) {
+      setTimeout(incrementCounter, interval);
+    }
+  };
+
+  incrementCounter();
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const counterElement = document.getElementById("span8");
+  const targetNumber = 80;
+  const duration = 1500; // Animation duration in milliseconds
+  const interval = duration / targetNumber;
+
+  let count = 0;
+
+  const incrementCounter = () => {
+    counterElement.textContent = count;
+    count++;
+
+    if (count <= targetNumber) {
+      setTimeout(incrementCounter, interval);
+    }
+  };
+
+  incrementCounter();
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const counterElement = document.getElementById("span50");
+  const targetNumber = 50;
+  const duration = 1000; // Animation duration in milliseconds
+  const interval = duration / targetNumber;
+
+  let count = 0;
+
+  const incrementCounter = () => {
+    counterElement.textContent = count;
+    count++;
+
+    if (count <= targetNumber) {
+      setTimeout(incrementCounter, interval);
+    }
+  };
+
+  incrementCounter();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const counterElement = document.getElementById("span20");
+  const targetNumber = 20;
+  const duration = 1000; // Animation duration in milliseconds
+  const interval = duration / targetNumber;
+
+  let count = 0;
+
+  const incrementCounter = () => {
+    counterElement.textContent = count;
+    count++;
+
+    if (count <= targetNumber) {
+      setTimeout(incrementCounter, interval);
+    }
+  };
+
+  incrementCounter();
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const counterElement = document.getElementById("span10");
+  const targetNumber = 50;
+  const duration = 2000; // Animation duration in milliseconds
+  const interval = duration / targetNumber;
+
+  let count = 0;
+
+  const incrementCounter = () => {
+    counterElement.textContent = count;
+    count++;
+
+    if (count <= targetNumber) {
+      setTimeout(incrementCounter, interval);
+    }
+  };
+
+  incrementCounter();
+});
